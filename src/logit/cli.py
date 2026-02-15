249cba66bf3adff6ed3e3e23466c5236aa0231db
yrevannavar <106887362+YashRevannavar@users.noreply.github.com>
2026-02-15 21:18:57 +0100

    feat(report): add command to list tracked time entries with optional JSON output

--- src/logit/cli.py
+++ src/logit/cli.py
@@ -8,9 +8,14 @@ def cli():
 
 # Import and register commands from other services
 from logit.control_commands.control_commands_service import start, stop  # noqa
-from logit.report_commands.report_commands_service import report, analyze  # noqa
+from logit.report_commands.report_commands_service import (  # noqa
+    report,
+    analyze,
+    ls,
+)
 
 cli.add_command(cmd=start)
 cli.add_command(cmd=stop)
 cli.add_command(cmd=report)
 cli.add_command(cmd=analyze)
+cli.add_command(cmd=ls)
